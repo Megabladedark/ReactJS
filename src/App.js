bfae92812c58956de76ac86aef3c608b4ca3820e
nguyen the <nguyenthe0202.1997@gmail.com>
2019-03-31 17:36:12 +0700

    Buoi 2: Component

--- src/App.js
+++ src/App.js
@@ -1,29 +1,85 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-            Hello worldssss
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Reactssssss
-          </a>
-        </header>
-      </div>
-    );
+const user = [
+  {
+    firstName: "Nguyễn",
+    lastName: "Thanh Thế",
+    email: "nguyenthe@gmail.com",
+  },
+  {
+    firstName: "Lê",
+    lastName: "Bích Phượng",
+    email: "bichphuong@gmail.com",
+  },
+  {
+    firstName: "Trần",
+    lastName: "Quang Thắng",
+    email: "",
+  },
+  {
+    firstName: "Nguyễn",
+    lastName: "Thanh Thế",
+    email: "nguyenthe@gmail.com",
+  },
+  {
+    firstName: "Lê",
+    lastName: "Bích Phượng",
+    email: "bichphuong@gmail.com",
+  },
+  {
+    firstName: "Trần",
+    lastName: "Quang Thắng",
+    // email: ,
+  },
+]
+
+function GetFullName(fn, ln){
+  const style = {
+    fontSize: 20,
+    marginBottom: "10px",
+    color: "#333",
   }
+  return <div style={style}>{fn} {ln}</div>
+}
+
+function GetEmail(mail){
+  const style = {
+    color: "#fff",
+    cursor: "pointer",
+    display: 'inline-block',
+    padding: "10px 20px",
+    border: "none",
+    borderRadius: 200,
+    backgroundColor: "#8ab4f7",
+    boxShadow: "0 2px 10px -5px rgba(0, 0, 0, 0.5)",
+    fontSize: 16,
+  }
+  
+  if (!mail){
+    mail = 'User không có mail'
+    style.backgroundColor = "red"
+  }
+  return <div style={style}>{mail}</div>
+}
+
+const User = (props) => {
+  const u = props.user
+  const style={
+    // position: "absolute",
+    marginTop: 70,
+    width: "100%",
+    textAlign: "center",
+  }
+
+  const element = <div style={style}>{GetFullName(u.firstName, u.lastName)}{GetEmail(u.email)}</div>
+  return element
+}
+
+const App = () =>{
+  const element = user.map(u => {
+    return <User user={u} />
+  })
+  return element
 }
 
 export default App;
